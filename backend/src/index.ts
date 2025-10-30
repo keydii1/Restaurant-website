fee47c1e41bc882be99c0a04252ed8815e04770c
Hồ Hoàng Sơn <hoson2k5@gmail.com>
2025-10-30 15:23:18 +0700

    Dowload dotenv, config tsconfig.json, test programs

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+
+dotenv.config();
+
+const app: Express = express();
+const PORT: number = Number(process.env.PORT) || 3000;
+
+app.use(bodyParser.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
