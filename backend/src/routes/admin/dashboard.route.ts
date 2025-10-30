2e0514a74d67cbccc9652e1081193f64bac9a824
Hồ Hoàng Sơn <hoson2k5@gmail.com>
2025-10-30 22:16:18 +0700

    Test dashboard route successfully

--- backend/src/routes/admin/dashboard.route.ts
+++ backend/src/routes/admin/dashboard.route.ts
@@ -1,3 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import * as controller from "../../controllers/admin/dashboard.controller";
+const router = express.Router();
+router.get("/", controller.index);
+export default router;
