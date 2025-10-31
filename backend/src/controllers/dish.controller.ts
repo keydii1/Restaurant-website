52bf3a1bb89dc499da3678becfd2953aa42022e4
Hồ Hoàng Sơn <hoson2k5@gmail.com>
2025-10-31 20:02:35 +0700

    Create sort feature for dishes

--- backend/src/controllers/dish.controller.ts
+++ backend/src/controllers/dish.controller.ts
@@ -3,20 +3,37 @@ import Dish from "../models/dish.model";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const getAllDishes = async (req: Request, res: Response) => {
+export const getDishes = async (req: Request, res: Response) => {
   try {
+    // define filter conditions
     interface DishCondition {
       deleted?: boolean;
       status?: string;
+      name?: RegExp;
     }
-
     const findCondition: DishCondition = {};
     const filterStatus = req.query.status as string;
-    console.log("Filter Status:", filterStatus);
+    const filterKeyword = req.query.keyword as string;
+    const sortKey = req.query.sortKey as string;
+    const sortValue = req.query.sortValue as string;
+    //  start status
     if (filterStatus) {
       findCondition.status = filterStatus as string;
     }
-    const dishes = await Dish.find(findCondition);
+    // end status
+    // start keyword
+    if (filterKeyword) {
+      const keywordRegex = new RegExp(filterKeyword, "i"); // Case-insensitive regex
+      findCondition.name = keywordRegex;
+    }
+    // end keyword
+    // sort
+    let sortCondition = {};
+    if (sortKey && sortValue) {
+      sortCondition[sortKey] = sortValue;
+    }
+
+    const dishes = await Dish.find(findCondition).sort(sortCondition);
     res.json({ message: "Dishes fetched successfully", data: dishes });
   } catch (error) {
     res.status(500).json({ message: "Internal server error", error });
--- backend/src/routes/dish.route.ts
+++ backend/src/routes/dish.route.ts
@@ -2,6 +2,6 @@ import express, { Express, Request, Response } from "express";
 import * as controller from "../controllers/dish.controller";
 const router = express.Router();
 
-router.get("/", controller.getAllDishes);
+router.get("/", controller.getDishes);
 
 export default router;
