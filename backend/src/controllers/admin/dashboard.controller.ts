c7656bfad0358f1d1c96f93b8207d4c92a382f56
Hồ Hoàng Sơn <hoson2k5@gmail.com>
2025-10-31 10:02:21 +0700

    Prepare a part of all data, cretae dishes model

--- backend/src/controllers/admin/dashboard.controller.ts
+++ backend/src/controllers/admin/dashboard.controller.ts
@@ -1,4 +1,12 @@
 import { Response, Request } from "express";
-export const index = (req: Request, res: Response) => {
-  res.json({ message: "Admin dashboard data" });
+import dotenv from "dotenv";
+dotenv.config();
+import Dish from "../../models/dish.model";
+export const index = async (req: Request, res: Response) => {
+  try {
+    const dist = await Dish.find();
+    res.json({ message: "Dashboard data fetched successfully", data: dist });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
 };
---
+++ backend/src/models/dish.model.ts
@@ -0,0 +1,45 @@
+import { Schema, model, Document } from "mongoose";
+
+interface IDish extends Document {
+  title: string;
+  description: string;
+  price: number;
+  discount: number;
+  images: string[];
+  categoryId: string;
+  status: string;
+  finalPrice: number;
+  rating: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const dishSchema = new Schema<IDish>(
+  {
+    title: { type: String, required: true },
+    description: {
+      type: String,
+      default: "",
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    discount: {
+      type: Number,
+      default: 0,
+    },
+    images: {
+      type: [String],
+      default: [],
+    },
+    categoryId: { type: String, required: true },
+    status: { type: String, required: true },
+    finalPrice: { type: Number, required: true },
+    rating: { type: Number, default: 0 },
+  },
+  { collection: "dishes", timestamps: true }
+);
+
+const Dish = model<IDish>("Dish", dishSchema);
+export default Dish;
