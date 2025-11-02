59bd8c9d94fe64ac422655deaf63c801d39905e8
Hồ Hoàng Sơn <hoson2k5@gmail.com>
2025-11-02 11:44:33 +0700

    Tạo thêm tính năng create và edit cho categories

--- backend/src/controllers/category.controller.ts
+++ backend/src/controllers/category.controller.ts
@@ -16,3 +16,21 @@ export const getCategories = async (req: Request, res: Response) => {
     });
   }
 };
+export const create = async (req: Request, res: Response) => {
+  try {
+    const newCategory = new Category(req.body);
+    await newCategory.save();
+    res.json({ message: "Category created successfully", data: newCategory });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+export const edit = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    await Category.updateOne({ _id: id }, req.body);
+    res.json({ message: "Category updated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
--- backend/src/routes/category.route.ts
+++ backend/src/routes/category.route.ts
@@ -3,4 +3,6 @@ import * as controller from "../controllers/category.controller";
 const router = Router();
 
 router.get("/", controller.getCategories);
+router.post("/create", controller.create);
+router.put("/edit/:id", controller.edit);
 export default router;
