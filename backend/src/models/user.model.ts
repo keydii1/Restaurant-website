541c92f4bed7e8c1386e0ad5ae4de5e9477da056
Hồ Hoàng Sơn <hoson2k5@gmail.com>
2025-11-03 23:32:28 +0700

    tạo users model

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,19 @@
+import { Document, Schema, model } from "mongoose";
+interface Iuser extends Document {
+  username: string;
+  email: string;
+  password: string;
+}
+const userSchema: Schema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+    collection: "users",
+  }
+);
+const User = model<Iuser>("User", userSchema);
+export default User;
