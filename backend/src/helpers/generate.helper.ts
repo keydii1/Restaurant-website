ee6d1e3e5d956dba76a506b8369dcf5412d8667c
Hồ Hoàng Sơn <hoson2k5@gmail.com>
2025-11-04 13:10:51 +0700

    create generate helper to genrerate string ramdon go assigned token variables

---
+++ backend/src/helpers/generate.helper.ts
@@ -0,0 +1,19 @@
+export const generateRandomString = (length: number): string => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+export const generateRandomNumber = (length: number): string => {
+  const characters = "0123456789";
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
--- backend/src/models/account.model.ts
+++ backend/src/models/account.model.ts
@@ -4,12 +4,22 @@ export interface IAccount extends Document {
   username: string;
   email: string;
   password: string;
+  role_id?: string;
+  avatar?: string;
+  deleted: boolean;
+  token?: string;
+  status?: string;
 }
 
 const accountSchema = new Schema<IAccount>({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  role_id: { type: String },
+  avatar: { type: String },
+  deleted: { type: Boolean, default: false },
+  token: { type: String },
+  status: { type: String, default: "active" },
 });
 
 export const Account = model<IAccount>("Account", accountSchema);
