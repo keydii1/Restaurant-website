2b4272d2492aa02c553654650ad1513df5795031
Hồ Hoàng Sơn <hoson2k5@gmail.com>
2025-11-14 14:55:44 +0700

    out storage and reuire pay

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -3,7 +3,12 @@ import dotenv from "dotenv";
 dotenv.config();
 export const connect = async (): Promise<void> => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URL, {
+      maxPoolSize: 5,
+      minPoolSize: 2,
+      socketTimeoutMS: 45000,
+      serverSelectionTimeoutMS: 5000,
+    });
     console.log("Connect DB successfully");
   } catch (err) {
     console.log("Connect DB fail:", err);
