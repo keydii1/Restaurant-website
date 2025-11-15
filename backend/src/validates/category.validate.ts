ae5ef2b95dca9aca10ad03cb06eaf62988a646b4
Hồ Hoàng Sơn <hoson2k5@gmail.com>
2025-11-15 23:53:04 +0700

    revised file that validate email and password

--- backend/src/validates/category.validate.ts
+++ backend/src/validates/category.validate.ts
@@ -1,32 +1,28 @@
 import { Request, Response } from "express";
-export const titleNotEmpty = (req: Request, res: Response, next: Function) => {
-  if (!req.body.title || req.body.title.trim() === "") {
-    return res.status(400).json({ message: "Title is required" });
+
+export const validateEmail = (req: Request, res: Response, next: Function) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; //
+  if (req.body.email && !emailRegex.test(req.body.email)) {
+    return res.status(400).json({ message: "Invalid email format" });
   }
   next();
 };
-export const tittleNotMoreThan30Chars = (
+export const validatePassword = (
   req: Request,
   res: Response,
   next: Function
 ) => {
-  if (req.body.title && req.body.title.length > 30) {
-    return res
-      .status(400)
-      .json({ message: "Title must not exceed 30 characters" });
+  const password = req.body.password;
+  if (password) {
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    // Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
+    if (!passwordRegex.test(password)) {
+      return res.status(400).json({
+        message:
+          "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character",
+      });
+    }
   }
-  next();
-};
-export const statusValid = (req: Request, res: Response, next: Function) => {
-  const validStatuses = ["active", "inactive"];
-  if (req.body.status && !validStatuses.includes(req.body.status)) {
-    return res
-      .status(400)
-      .json({ message: "Status must be either active or inactive" });
-  }
-  next();
-};
-export const test = (req: Request, res: Response, next: Function) => {
-  console.log("Test middleware executed");
   next();
 };
