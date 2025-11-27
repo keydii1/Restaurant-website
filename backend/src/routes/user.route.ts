60fb497fdee1f82d846b3d964088693a955bd9e6
Hồ Hoàng Sơn <hoson2k5@gmail.com>
2025-11-27 16:31:27 +0700

    add validate for user route

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import * as controller from "../controllers/user.controller";
-import * as validator from "../validates/user.validate";
+import * as userValidate from "../validates/user.validate";
 const router = Router();
 import {
   validateEmail,
@@ -8,11 +8,35 @@ import {
 } from "../validates/EmailAndPassword.validate";
 import { authAdmin } from "../auth/checkAuth.auth";
 router.get("/", authAdmin, controller.getUsers);
-router.post("/register", controller.register);
-router.post("/login", controller.login);
-router.post("/forgot-password", controller.forgotPassword);
+router.post(
+  "/register",
+  userValidate.usernameNotEmpty,
+  validateEmail,
+  userValidate.emailValid,
+  userValidate.emailExistCheck,
+  validatePassword,
+  userValidate.passwordRequirements,
+  controller.register
+);
+router.post(
+  "/login",
+  validateEmail,
+  userValidate.emailValid,
+  controller.login
+);
+router.post(
+  "/forgot-password",
+  validateEmail,
+  userValidate.emailValid,
+  controller.forgotPassword
+);
 router.post("/verify-otp", controller.verifyOtp);
-router.patch("/reset-password", controller.resetPassword);
+router.patch(
+  "/reset-password",
+  validatePassword,
+  userValidate.passwordRequirements,
+  controller.resetPassword
+);
 router.get("/logout", controller.logout);
 router.post("/refresh-token", controller.refreshToken);
 router.get("/auth/google", controller.googleAuth);
