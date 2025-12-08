3f8e0c7ffe046b3e769669d52fb3af9cff70b35b
Hồ Hoàng Sơn <hoson2k5@gmail.com>
2025-12-08 23:02:02 +0700

    rollback fomer commit

--- backend/src/routes/table.route.ts
+++ backend/src/routes/table.route.ts
@@ -4,7 +4,6 @@ import * as tableValidate from "../validates/table.validate";
 const router = Router();
 import { auth, authAdmin } from "../auth/checkAuth.auth";
 router.get("/", auth, controllers.getAllTables);
-router.get("/available", auth, controllers.getAvailableTables);
 router.post(
   "/create",
   authAdmin,
